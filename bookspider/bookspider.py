d8c68c261ff038ed942f92c80da23e95b6fdbfad
Reuven Lerner <reuven@lerner.co.il>
2024-08-26 20:37:29 +0300

    gitautosync update 71

--- bookspider/bookspider.py
+++ bookspider/bookspider.py
@@ -7,8 +7,10 @@ class BookSpider(scrapy.Spider):
     def parse(self, response):
 
         for one_quote in response.css('article.product_pod'):
-            quote_text = one_quote.css('.text::text').extract_first()
-            author_name = one_quote.css('.author::text').extract_first()
+            # quote_text = one_quote.css('.text::text').extract_first()
+            # author_name = one_quote.css('.author::text').extract_first()
 
-            yield {'text': quote_text,
-                   'author':author_name}
+            # yield {'text': quote_text,
+            #        'author':author_name}
+
+            yield {}
