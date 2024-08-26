3a88f7a727dafa3d8c94573a8c985435c7fed204
Reuven Lerner <reuven@lerner.co.il>
2024-08-26 20:11:22 +0300

    gitautosync update 52

--- scraper1/scraper1.py
+++ scraper1/scraper1.py
@@ -3,3 +3,7 @@ import scrapy
 class QuoteSpider(scrapy.Spider):
     name = 'quote-spider'
     start_urls = ['https://quotes.toscrape.com']
+
+    def parse(self, response):
+        # this method should look for one or more values in the
+        # response, using CSS selectors.
