a86a5f8097e4ddf21f193b63285228426e753b40
Reuven Lerner <reuven@lerner.co.il>
2024-08-26 20:35:40 +0300

    gitautosync update 67

---
+++ bookspider/bookspider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quote-spider'
+    start_urls = ['https://quotes.toscrape.com']
+
+    def parse(self, response):
+        # this method should look for one or more values in the
+        # response, using CSS selectors.
+
+        for one_quote in response.css('div.quote'):
+            # grab the item with a "text" class (i.e., .text)
+            # grab the text from within the text (i.e., ::text)
+            quote_text = one_quote.css('.text::text').extract_first()
+            author_name = one_quote.css('.author::text').extract_first()
+
+            yield {'text': quote_text,
+                   'author':author_name}
