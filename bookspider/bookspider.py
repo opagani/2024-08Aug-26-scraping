e3497da89771c3956154aa24b6b7106a193c5f85
Reuven Lerner <reuven@lerner.co.il>
2024-08-26 20:46:10 +0300

    gitautosync update 89

--- bookspider/bookspider.py
+++ bookspider/bookspider.py
@@ -6,6 +6,7 @@ class BookSpider(scrapy.Spider):
 
     def parse(self, response):
 
+        # Find each book, iterate and yield
         for one_book in response.css('article.product_pod'):
 
             title_text = one_book.css('h3 > a::text').extract_first()
@@ -13,3 +14,9 @@ class BookSpider(scrapy.Spider):
 
             yield {'title':title_text,
                    'price':price}
+
+        # Find the "next" button, and yield that for additional scraping
+        next_page = response.css('.next > a:nth-child(1)').extract_first()
+
+        if next_page:   # if there is a next_page button...
+            yield scrapy.Request(response.urljoin(next_page))
